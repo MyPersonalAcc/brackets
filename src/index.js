56f4d226241d634631e0d18c0a3ee65585d53fd3
User <givemeaccplz21@gmail.com>
2019-03-17 16:47:28 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var brackets = str.split('');
+  var index = 0;
+  while(index <str.length){
+    for (let i = 0; i < str.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        var bracketsOpen = bracketsConfig[j][0];
+        var bracketsClose = bracketsConfig[j][1];
+        if (brackets[i] == bracketsOpen) {
+          if (brackets[i + 1] == bracketsClose) {
+            brackets.splice(i, 2);
+            i -= 2;
+          }
+          else {
+            index++;
+          }
+        }
+      }
+    }
+    index++;
+  }
+  if (brackets.length == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
